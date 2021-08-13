df6c0a23b4cda56acbbde90a3a5598b74acb1a4f
idk-12 <3190103830@zju.edu.cn>
2021-08-13 18:49:55 +0800

    finished proj1b

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -4,6 +4,8 @@ public class OffByN implements CharacterComparator {
     public OffByN(int N) {
         n = N;
     }
+
+    @Override
     public boolean equalChars(char x, char y) {
         if ((x - y) == n || (y - x) == n) {
             return true;
